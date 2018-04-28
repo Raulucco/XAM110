7479544065ba6be8ed54a8f22ec3803c18e4ae0f
Raulucco <rauluccocm@gmail.com>
2018-04-28 10:34:47 +0100

    Load JSON file content with Share Project

--- Exercise 1/Start/MyTunes.iOS/MyTunesViewController.cs
+++ Exercise 1/Start/MyTunes.iOS/MyTunesViewController.cs
@@ -1,4 +1,5 @@
 using UIKit;
+using System.Linq;
 
 namespace MyTunes
 {
@@ -11,12 +12,18 @@ namespace MyTunes
 			TableView.ContentInset = new UIEdgeInsets (20, 0, 0, 0);
 		}
 
-		public override void ViewDidLoad()
+		public async override void ViewDidLoad()
 		{
 			base.ViewDidLoad();
 
-			TableView.Source = new ViewControllerSource<string>(TableView) {
-				DataSource = new string[] { "One", "Two", "Three" },
+			//TableView.Source = new ViewControllerSource<string>(TableView) {
+			//	DataSource = new string[] { "One", "Two", "Three" },
+			//};
+			var data = await SongLoader.Load();
+			TableView.Source = new ViewControllerSource<Song>(TableView) {
+				DataSource = data.ToList(),
+				TextProc = s => s.Name,
+				DetailTextProc = s => s.Artist + " - " + s.Album,
 			};
 		}
 	}
